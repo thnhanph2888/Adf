ecc03604e1c024be68bc1969791547406b796b81
Nhan <thanhnhanphan2888@gmail.com>
2022-11-23 20:56:26 +0700

    fix

--- src/s14_thuat_toan_sap_xep/excercise/illustration_insert_sort_algorithm/InsertSort.java
+++ src/s14_thuat_toan_sap_xep/excercise/illustration_insert_sort_algorithm/InsertSort.java
@@ -4,8 +4,9 @@ package excercise.illustration_insert_sort_algorithm;
 import java.util.Arrays;
 
 public class InsertSort {
-    static int[] list = {6,7,4,2,9,12,1};
-    static String[] listIndex = {" "," "," "," "," "," "," "};
+    static int[] list = {6, 7, 4, 2, 9, 12, 1};
+    static String[] listIndex = {" ", " ", " ", " ", " ", " ", " "};
+
     static void insertSort(int[] list) {
         int pos, x;
         boolean isTrue = false;
@@ -19,14 +20,13 @@ public class InsertSort {
                 System.out.println("Gắn " + list[pos] + " thành " + list[pos - 1]);
                 list[pos] = list[pos - 1];
                 System.out.println(Arrays.toString(list));
-                listIndex[pos] = " ";
                 listIndex[pos - 1] = x + "";
                 System.out.println(Arrays.toString(listIndex));
                 listIndex[pos - 1] = " ";
                 pos--;
                 isTrue = true;
             }
-            if (!isTrue){
+            if (!isTrue) {
                 System.out.println("So sánh " + x + " lớn hơn " + list[pos - 1] + " nên giữ nguyên");
                 System.out.println(Arrays.toString(list));
                 System.out.println("");
